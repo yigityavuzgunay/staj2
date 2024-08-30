3453024a5d2da624996f84417aa2bf752dda8e31
yigityavuz67 <yavuzg338@gmail.com>
2024-08-31 01:02:40 +0300

    staj
    saldırı sistemi

--- staj projem/Assets/Scripts/EnemyManager.cs
+++ staj projem/Assets/Scripts/EnemyManager.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     public float health;
     public float damage;
 
+    bool colliderBusy = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +22,44 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-
+        if (other.tag == "Player" &&!colliderBusy)
+        {
+            colliderBusy = true;
+           other.GetComponent<PlayerManager>().GetDamage(damage);
+        }
+        else if (other.tag == "Bullet")
+        {
+            GetDamage(other.GetComponent<BulletManager>().bulletDamage);
+            Destroy(other.gameObject);
+        }
+       
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            colliderBusy = false;
+        }
+    }
+    public void GetDamage(float damage)
+    {
+        if ((health - damage) >= 0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+        }
+        AmIdead();
+    }
 
+    void AmIdead()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- staj projem/Assets/Scripts/PlayerManager.cs
+++ staj projem/Assets/Scripts/PlayerManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public float health, bulletSpeed;
+
+    bool dead = false;
+
+    Transform muzzle;
+
+    public Transform bullet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        muzzle = transform.GetChild(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            ShootBullet();
+        }
     }
+
+    public void GetDamage(float damage)
+    {
+        if ((health - damage) >= 0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+        }
+        AmIdead();
+    }
+
+    void AmIdead()
+    {
+        if (health <= 0)
+        {
+            dead = true;
+        }
+    }
+
+    void ShootBullet()
+    {
+        Transform tempBullet;
+        tempBullet = Instantiate(bullet, muzzle.position, Quaternion.identity);
+        tempBullet.GetComponent<Rigidbody2D>().AddForce(muzzle.forward * bulletSpeed);
+   }
+  
 }
